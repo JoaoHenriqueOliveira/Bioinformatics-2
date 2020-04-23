786500381350a7ff8ce4123d11825513d4bb4836
JoaoHenriqueOliveira <joaojhro@gmail.com>
2020-04-23 19:18:16 +0200

    added gitignore for txt files

--- week2/EulerianCycle.py
+++ week2/EulerianCycle.py
@@ -7,22 +7,40 @@ def count_edges(graph):
     
     return count
 
+def count_nodes(graph):
+    nodes = []
+    for node in graph:
+        if not node in nodes:
+            nodes.append(node)
+        adj = graph[node]
+        for elem in adj:
+            if not elem in nodes:
+                nodes.append(elem)
+    
+    return len(nodes), nodes
+
 def EulerianPath(graph):
-    n = len(graph)
-    in_deg = [0 for i in range(n)]
-    out_deg = [0 for i in range(n)]
+    n, nodes = count_nodes(graph)
+    
+    in_deg = {}
+    out_deg = {}
+    
+    for node in nodes:
+        in_deg[node] = 0
+        out_deg[node] = 0
+        
     path = []
     
-    return FindEulerianPath(in_deg, out_deg, graph, n, path)
+    return FindEulerianPath(in_deg, out_deg, graph, n, nodes, path)
 
-def FindEulerianPath(in_deg, out_deg, graph, n, path):
+def FindEulerianPath(in_deg, out_deg, graph, n, nodes, path):
     m = count_edges(graph)
     in_deg, out_deg = CountInOutDegree(in_deg, out_deg, graph)
     
-    if not HasEulerianPath(in_deg, out_deg, n):
+    if not HasEulerianPath(in_deg, out_deg):
         return -1
-    #start = findStartNode(in_deg, out_deg, n)
-    start = 1140
+    
+    start = findStartNode(in_deg, out_deg)
     path = dfs(start, out_deg, graph, path)
     
     if len(path) == m + 1:
@@ -30,14 +48,14 @@ def FindEulerianPath(in_deg, out_deg, graph, n, path):
     
     return -1
 
-def findStartNode(in_deg, out_deg, n):
+def findStartNode(in_deg, out_deg):
     start = 0
     
-    for i in range(n):
-        if out_deg[i] - in_deg[i] == 1:
-            return i
-        if out_deg[i] > 0:
-            start = i
+    for node in in_deg:
+        if out_deg[node] - in_deg[node] == 1:
+            return node
+        if out_deg[node] > 0:
+            start = node
     
     return start
 
@@ -50,7 +68,6 @@ def dfs(at, out_deg, graph, path):
     path.append(at)
     return path
     
-
 def CountInOutDegree(in_deg, out_deg, graph):
     
     for node in graph:
@@ -61,15 +78,15 @@ def CountInOutDegree(in_deg, out_deg, graph):
     
     return in_deg, out_deg
 
-def HasEulerianPath(in_deg, out_deg, n):
+def HasEulerianPath(in_deg, out_deg):
     start, end = 0, 0
     
-    for i in range(n):
-        if in_deg[i] - out_deg[i] > 1 or out_deg[i] - in_deg[i] > 1:
+    for node in in_deg:
+        if in_deg[node] - out_deg[node] > 1 or out_deg[node] - in_deg[node] > 1:
             return False
-        elif in_deg[i] - out_deg[i] == 1:
+        elif in_deg[node] - out_deg[node] == 1:
             end += 1
-        elif out_deg[i] - in_deg[i] == 1:
+        elif out_deg[node] - in_deg[node] == 1:
             start += 1
             
     return(start == 0 and end == 0) or (start == 1 and end == 1)
@@ -80,15 +97,13 @@ class EulerianCycleTest(unittest.TestCase):
         self.assertEqual(count_edges({0: [3], 1: [0], 2: [1, 6], 3: [2], 4: [2], 5: [4], 6: [5, 8], 7: [9], 8: [7], 9: [6]}), 12)
         
     def test_has_eulerian_path(self):
-        self.assertEqual(HasEulerianPath([1, 1, 2, 1, 1, 1, 2, 1, 1, 1], [1, 1, 2, 1, 1, 1, 2, 1, 1, 1], 10), True)
+        self.assertEqual(HasEulerianPath([1, 1, 2, 1, 1, 1, 2, 1, 1, 1], [1, 1, 2, 1, 1, 1, 2, 1, 1, 1]), True)
         
 if __name__ == "__main__":
     #unittest.main()
     
     lines = open("test.txt", "r").readlines()
     graph = {}
-    
-    
     for line in lines:
         tmp = ''
         for index, char in enumerate(line):
@@ -99,13 +114,14 @@ if __name__ == "__main__":
                     graph[nbr].append(int(tmp))
                     tmp = ""
             if char == '>':
-                #print(tmp)
                 nbr = int(tmp)
                 graph[nbr] = []
                 tmp = ''
-              
+
     res = EulerianPath(graph)
+    
     for node in res:
         print(f"{node}->", end = "")
     print()
+    
     pass
